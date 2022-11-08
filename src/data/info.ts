4994c1851e2fd44758466eeeac164f61675b7b79
oletilwe12 <loatemophuting@gmail.com>
2022-11-08 09:29:44 +0200

    first commit

---
+++ src/data/info.ts
@@ -0,0 +1,100 @@
+export const info = {
+  baseUrl: "https://oletilwe.netlify.app",
+  name: "Oletilwe Mophuting",
+  jobDescription: "Web Developer",
+  about: `I am a software developer based in Cape Town, South Africa. 
+   I enjoy creating things that live on the internet, whether that be websites, applications, or anything in between. 
+   I am a motivated student with a huge computer background. I created my first website in 2015 using Wix. 
+   I have successfully completed several web projects from scratch. 
+   Yet, I learn continuously and never stop exploring new technologies.
+   So, I have also gained knowledge of Node JS, C# & .Net, Mongo DB, Python,
+   firebase and with time.
+  `,
+
+  experience: [
+    {
+      name: "Logicalis SA",
+      location: "Tygerberg, Cape Town, South Africa",
+      startDate: "July 2022",
+      endDate: "Present",
+      description: [
+        "✓ A project based learning experience. ",
+        "✓ Working with HTML, CSS & Javascript/Typescript ",
+        "✓ On frameworks we use ReactJS ",
+        "✓ Backend and DB we use NodeJS and Firebase ",
+      ],
+    },
+
+    {
+      name: "GNO Enterprise",
+      location: "Pretoria, Gauteng, South Africa",
+      startDate: "Jan 2019",
+      endDate: "Feb 2022",
+      description: [
+        "✓ Was a computer technician at my family’s business. ",
+        "✓ Help and training of other colleagues",
+        "✓ Others tasks connected to the nature of the job",
+        "✓ Had to leave due to school ",
+      ],
+    },
+
+  ],
+
+  education: [
+    {
+      name: "Zaio, Full Stack Developer",
+      location: "Cape Town, South Africa",
+      startDate: "2022",
+      endDate: "2023",
+      description: [
+        "A project based learning experience","Working with HTML, CSS & Javascript/Typescript", "On frameworks we use ReactJS", "Backend and DB we use NodeJS and Firebase"
+      ],
+    },
+    {
+      name: "IT Academy, Systems Development NQF5",
+      location: "Cape Town, South AFrica",
+      startDate: "2022",
+      endDate: "2022",
+      description: ["MTA Software & Database Fundamentals", "MICROSOFT CERTIFIED SOLUTIONS ASSOCIATE (MCSA): WEB APPLICATIONS", "BH0-010 ISTQB BCS CERTIFIED TESTER FOUNDATIONS LEVEL (CTFL)", "CompTIA A+"],
+    },
+  ],
+
+  socialMedia: {
+    facebook: "https://www.facebook.com/nasdaqking.y2g",
+    twitter: "https://twitter.com/Lloyd_codes",
+    github: "https://github.com/oletilwe12",
+    email: "loatemophutingd@gmail.com",
+    linkedin: "https://www.linkedin.com/in/lloyd-oletilwe-mophuting-030748220/",
+  },
+
+  projects: [
+    {
+      title: "Youtube 2.0",
+      isFeatured: true,
+      thumbnail: "https://i.ytimg.com/vi/FHTbsZEJspU/hqdefault.jpg?sqp=-oaymwEXCNACELwBSFryq4qpAwkIARUAAIhCGAE=&rs=AOn4CLAOrlUtYkzoHvOlNc2aCBUqrkx9bw",
+      githubUrl: "https://github.com/oletilwe12/youtube_clone",
+      liveUrl: "https://lloyd-tube.netlify.app/",
+    },
+    {
+      title: "Twitter Clone",
+      isFeatured: true,
+      thumbnail: "http://res.cloudinary.com/ak-124210/image/upload/v1642434078/yjnzwmihmhghxkwnb6n9.png",
+      githubUrl: "https://github.com/oletilwe12/twitter-clone",
+      liveUrl: "https://sunny-raindrop-89f7f9.netlify.app/",
+    },
+    {
+      title: "Tesla Landing Page",
+      isFeatured: true,
+      thumbnail: "https://zaiocontent.s3.eu-west-2.amazonaws.com/images+of+courses/Tesla.png",
+      githubUrl: "https://github.com/oletilwe12/Tesla-clone.git",
+      liveUrl: "https://playful-starburst-29df91.netlify.app",
+    },
+    {
+      title: "Exercise ZA",
+      isFeatured: true,
+      thumbnail: "https://i.ytimg.com/vi/KBpoBc98BwM/hq720.jpg?sqp=-oaymwEcCNAFEJQDSFXyq4qpAw4IARUAAIhCGAFwAcABBg==&rs=AOn4CLB8e-qu0gXwVVES-HxQG_838VIwhQ",
+      githubUrl: "https://github.com/oletilwe12/gym_exercises",
+      liveUrl: "https://exercise-za.netlify.app/",
+    },
+  ], 
+};
